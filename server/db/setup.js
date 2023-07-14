62681275ee634cf3b2acc266d95fccfb46519c00
Patrick Nilan <nilan.patrick@gmail.com>
2023-07-13 17:16:48 -0700

    updated reference to env variable PGPORT

--- server/db/setup.js
+++ server/db/setup.js
@@ -6,7 +6,7 @@ const db = new Client({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: process.env.DBPORT
+  port: process.env.PGPORT
 });
 
 db.connect()
